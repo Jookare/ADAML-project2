718cfa4aeffacb5aba590a666708eeb55246c243
Jookare <134264039+Jookare@users.noreply.github.com>
2024-11-11 19:17:50 +0200

    Add decompositions

--- utils.py
+++
@@ -1,9 +0,0 @@
-def remove_outliers(df_in, col_name):
-        q1 = df_in[col_name].quantile(0.25)
-        q3 = df_in[col_name].quantile(0.75)
-        iqr = q3 - q1
-        fence_low  = q1 -2 *iqr
-        fence_high = q3 +2 *iqr
-        df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
-        
-        return df_out, fence_low, fence_high
