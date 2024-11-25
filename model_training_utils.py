00af6ac8f152fabac059c69d39b2eb89cd34b945
akaronen <alex.karonen@gmail.com>
2024-11-25 16:43:09 +0200

    Initialized utility package for model training

--- model_training_utils.py
+++ model_training_utils.py
@@ -1,32 +1,65 @@
 import torch
 import pandas as pd
+import numpy as np
 from torch.utils.data import Dataset
 
 
 class TimeSeriesDataset(Dataset):
     """
     Arguments:
-        path (string): path of the csv file (assuming the csv file has a column named "date" and a column named "target")
-
+        df_or_path (pd.DataFrame or str): dataframe or path to the csv file
+        window_size (int): size of the input sequence
+        horizon (int): number of steps ahead to predict
+        stride (int): stride for the windowing
     """
 
-    def __init__(self, path):
-        df = pd.read_csv(path)
-        df["date"] = pd.to_datetime(df["date"])
-        self.X = df.drop(["date"], ["target"], axis=1)
+    def __init__(self, df_or_path=None, window_size=7, horizon=1, stride=1):
+
+        if isinstance(df_or_path, str):
+            df = pd.read_csv(df_or_path)
+        elif isinstance(df_or_path, pd.DataFrame):
+            df = df_or_path
+        else:
+            raise ValueError(
+                f"'df_or_path' must be a pandas DataFrame or a string, got {type(df_or_path)}"
+            )
+        self.features = df.drop(["date"], axis=1)
         self.date = df["date"]
-        self.Y = df["target"]
+        self.X, self.Y = self._sequencing(
+            window_size=window_size, horizon=horizon, stride=stride
+        )
 
     def __len__(self):
         return len(self.date)
 
     def __getitem__(self, idx):
-        X = torch.tensor(self.X.iloc[idx, :].values)
-        Y = torch.tensor(self.Y.iloc[idx])
+        X = self.X[idx]
+        Y = self.Y[idx]
         return X, Y
 
     def get_date(self):
         return self.date
+
+    def _sequencing(self, window_size, horizon, stride=1):
+        """
+        Arguments:
+            window_size (int): size of the window
+            horizon (int): number of steps ahead to predict
+            stride (int): stride for the windowing
+        """
+        n_samples, n_features = self.features.shape
+        sequences = range(0, n_samples - window_size - horizon + 1, stride)
+        inp, target = np.empty((len(sequences), window_size, n_features)), np.empty(
+            (len(sequences), horizon)
+        )
+        for i in sequences:
+            inp[i, :, :] = self.features.iloc[i : i + window_size, :].values
+            target[i, :] = (
+                self.features["humidity"]
+                .iloc[i + window_size : i + window_size + horizon]
+                .values
+            )
+        return torch.from_numpy(inp), torch.from_numpy(target)
 
 
 def train_model(model, dataloader, criterion, optimizer, num_epochs=25, device="cpu"):
